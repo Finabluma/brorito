aebd6f92b27bafced9fac00f5ec9e2bcc72a0161
finabluma <oscar.francino@finabluma.com>
2022-06-22 11:03:53 +0200

    query restructured

--- graphql/queries/prensa.js
+++ graphql/queries/prensa.js
@@ -22,4 +22,61 @@ const PrensaQuery = gql`
     }
   }
 `
-export default PrensaQuery
+const PrensaSlug = gql`
+  query Prensa($slug: String){
+  prensas(filters:{slug:{eq:$slug}}){
+      data{
+        id
+        attributes{
+          titulo
+          imagen{
+            data{
+              id
+              attributes{
+                provider_metadata
+                alternativeText
+              }
+            }
+          }
+          contenido
+          video{
+            URL
+            nombre
+          }
+          etiquetas{
+            data{
+              id
+              attributes{
+                nombre
+                slug
+              }
+            }
+          }
+          prensarelacionada{
+            id
+            intro
+            prensas{
+              data{
+                id
+                attributes{
+                  titulo
+                  slug
+                  imagen{
+                    data{
+                      id
+                      attributes{
+                        provider_metadata
+                        alternativeText
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+export { PrensaQuery, PrensaSlug }
