ea126e107df72db83195c14bda1623a62db69962
finabluma <oscar.francino@finabluma.com>
2022-06-22 11:56:47 +0200

    appseccion

---
+++ components/AppSeccion.vue
@@ -0,0 +1,37 @@
+<template>
+  <div :class="{ inner_content: contacta }">
+    <h1>{{ seccionData.attributes.titulo }}</h1>
+    <div
+      :class="{ lead: lead }"
+      v-html="seccionData.attributes.contenido"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "pagesections",
+  props: {
+    seccion: Object,
+  },
+  computed: {
+    seccionData() {
+      return this.seccion.data;
+    },
+    lead() {
+      return this.$route.name === "reservas";
+    },
+    contacta() {
+      return this.$route.name === "contacta";
+    },
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+.lead {
+  @apply font-hero
+    font-light
+    text-paragraph;
+}
+</style>
